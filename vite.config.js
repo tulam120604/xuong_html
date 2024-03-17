a1a121581cd6913e562ad433abb4503e35ee085a
tulam120604 <lamctph35369@fpt.edu.vn>
2024-03-17 22:00:44 +0700

    updatev3

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'Desktop/Home/Home.html'),
+                shop: resolve(__dirname, 'Desktop/Shop/Shop.html'),
+                deital: resolve(__dirname, 'Desktop/Deital/Deital.html'),
+                cart: resolve(__dirname, 'Desktop/Cart/Cart.html'),
+                checkout: resolve(__dirname, 'Desktop/CheckOut/CheckOut.html'),
+            },
+        },
+    },
+})
