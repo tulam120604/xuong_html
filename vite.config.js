5da041d487cb377a68a878df9abe09e32aa691dd
tulam120604 <lamctph35369@fpt.edu.vn>
2024-03-17 22:23:19 +0700

    update v10

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, './Home.html'),
+                main: resolve(__dirname, '.Home.html'),
                 shop: resolve(__dirname, './Shop.html'),
                 deital: resolve(__dirname, './Deital.html'),
                 cart: resolve(__dirname, './Cart.html'),
